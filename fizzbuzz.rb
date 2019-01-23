83e0814b986415a52a18aba63f8a515b8bca0824
Jason Fournier <jmf4nier@hotmail.com>
2019-01-23 16:59:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizz_buzz(num)
+  if num % 3 == 0 && num % 5 == 0 
+    puts "Fizzbuzz"
